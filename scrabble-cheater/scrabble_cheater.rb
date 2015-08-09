4ee6ac00c57b68878a7f7fabda89bc91d712d3d7
Pete Higgins <pete@peterhiggins.org>
2015-08-09 15:24:32 -0700

    Off-by-one error. :crying_cat_face:

--- scrabble-cheater/scrabble_cheater.rb
+++ scrabble-cheater/scrabble_cheater.rb
@@ -14,7 +14,7 @@ map = File.readlines(DICT).
   map(&:downcase).
   group_by {|word| word.each_char.sort.join }
 
-subsets = input_chars.size.
+subsets = (1..input_chars.size).to_a.
   flat_map { |n| input_chars.combination(n).to_a }.
   map(&:sort).
   map(&:join).
