be9ce262817b0676842430df55344a7748f79c46
Pete Higgins <pete@peterhiggins.org>
2015-08-09 15:04:08 -0700

    Use online scrabble dictionary.

--- scrabble-cheater/scrabble_cheater.rb
+++ scrabble-cheater/scrabble_cheater.rb
@@ -1,10 +1,15 @@
+# Download from:
+# https://raw.githubusercontent.com/jonbcard/scrabble-bot/master/src/dictionary.txt
+DICT = 'dictionary.txt'
+#DICT= '/usr/share/dict/words'
+
 input_chars = if ARGV.empty?
   7.times.map { ('a'..'z').to_a.sample }
 else
   ARGV.first.each_char.to_a
 end
 
-map = File.readlines("/usr/share/dict/words").
+map = File.readlines(DICT).
   map(&:chomp).
   map(&:downcase).
   group_by {|word| word.each_char.sort.join }
