5d1366792446d11aa784225b99bc4b3ae1848a9e
Pete Higgins <pete@peterhiggins.org>
2015-09-14 23:33:51 -0700

    wat

--- pascal-triangle/trianglizer.rb
+++ pascal-triangle/trianglizer.rb
@@ -3,14 +3,14 @@ class Trianglizer
     gap = triangle.flatten.map(&:to_s).max_by {|s| s.size }.size
     length = triangle.size * gap * 2
 
-    triangle.map {|row| row.join(" " * gap).center(length) }.join("\n")
+    triangle.map {|row| row.join(" " * gap).center(length).rstrip }.join("\n")
   end
 end
 
 if $0 == __FILE__
   require 'minitest/autorun'
 
-  describe Triangulizer do
+  describe Trianglizer do
     it "does the thing" do
       input = [
         [1],
@@ -21,7 +21,7 @@ if $0 == __FILE__
         [1, 5,  10, 10, 5,  1],
       ]
 
-      expected = <<-TRIANGLE.strip
+      expected = <<-TRIANGLE.rstrip
            1
           1  1
         1  2  1
@@ -30,7 +30,7 @@ if $0 == __FILE__
    1  5  10  10  5  1
       TRIANGLE
 
-      assert_equal expected, IterativePascalsTriangle.generate(8)
+      assert_equal expected, Trianglizer.trianglify(input)
     end
   end
 end
